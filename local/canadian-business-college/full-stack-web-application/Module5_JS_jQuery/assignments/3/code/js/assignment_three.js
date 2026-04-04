cb92f00c07457bf928f232c4be692ebc4a0667a3
David H. Watson <hiranwatson@gmail.com>
2026-04-03 23:10:34 -0400

    Created a new function and two new global variables to process the returned university data
    The function extracts only the university name, its state/province, and its domains.

--- local/canadian-business-college/full-stack-web-application/Module5_JS_jQuery/assignments/3/code/js/assignment_three.js
+++ local/canadian-business-college/full-stack-web-application/Module5_JS_jQuery/assignments/3/code/js/assignment_three.js
@@ -6,6 +6,16 @@ const api_obj = {
   country_name: "",
 };
 
+/** Global object to represent each university */
+let university_info = {
+  name: "",
+  state_province: "",
+  domains: [],
+};
+
+/** Global variable to hold all the universities */
+let uni_data_arr = [];
+
 /**
  * Fetch function to make the API call and get the returned data.
  * @param {string} url: The url to use in the fetch command, passed in as a string
@@ -17,8 +27,8 @@ async function fetchData(url) {
   try {
     response_data = await fetch(url);
     university_list = await response_data.json();
-    console.log(response_data);
-    console.log(university_list);
+
+    processUniversities(university_list);
   } catch (error) {
     console.log(error);
   }
@@ -30,6 +40,21 @@ async function fetchData(url) {
 function getCountryName() {
   const select_country = document.querySelector("#select-country");
   return select_country.value;
+}
+
+/**
+ * Function to process the returned universities
+ * @param {object} university_list : an object that represents a list of universities from the API http://universities.hipolabs.com
+ */
+function processUniversities(university_list) {
+  const section_uni_list = document.querySelector(".section-university-list");
+
+  for (let university of university_list) {
+    university_info.name = university["name"];
+    university_info.state_province = university["state-province"];
+    university_info.domains = university["domains"];
+    uni_data_arr.push(university_info);
+  }
 }
 
 let submit_btn = document.querySelector("#submit-btn");
