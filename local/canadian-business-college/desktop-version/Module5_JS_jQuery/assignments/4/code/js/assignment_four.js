4bdf0cd519ec29c5c3a6538c6ebe9b10a1d530af
David H. Watson <hiranwatson@gmail.com>
2026-04-07 07:31:37 -0400

    Set up initial fetching logic for the random dog API
    Created more reference constants to the divs for the API data. Also created two global constants to hold user alert messages.
    
    Created the click event listener for the "Fetch Doggie" button by using the jQuery on() method. In the passed function, two other functions are called. See below:
    
    Function name:
    updateUserOnAttempt
    Function purpose:
    Adds a paragraph element to the api data div to let the user know that the data is attempting to be fetched. It was decided to add this since the jQuery get() function can have a delay. jQuery is used to create the paragraph element, set its text, and append it to the div.
    
    Function name:
    fetchAPIData
    Function purpose:
    This function does the actual fetching of the data. It takes in a boolean value which it uses to determine which API call to make. The jQuery $.get() method is used and the fail() method is added to alert the user if the API call failed.

--- local/canadian-business-college/desktop-version/Module5_JS_jQuery/assignments/4/code/js/assignment_four.js
+++ local/canadian-business-college/desktop-version/Module5_JS_jQuery/assignments/4/code/js/assignment_four.js
@@ -5,5 +5,45 @@ const ruser_api_url = "https://randomuser.me/api/";
 /** Global constants to hold the necessary class and id names that will be referenced with jQuery. */
 const jq_submit_btn_dog = "#submit-btn-dog";
 const jq_submit_btn_human = "#submit-btn-human";
-const jq_api_div_dog = "#api-div-dog";
-const jq_api_div_human = "#api-div-human";
+const jq_api_div_dog = "#api-data-dog";
+const jq_api_div_human = "#api-data-human";
+const jq_api_div_data = ".api-data";
+
+/** Global constants to act as user alert messages */
+const fetch_api_err = "The fetch call failed! Please try again...";
+const fetch_api_attempt = "Attempting the fetch...";
+
+/** Let the user know the "get" call is happening. This is helpful since a jQuery get call can take a few milliseconds or seconds to complete. */
+function updateUserOnAttempt() {
+  // Create new paragraph
+  let user_para = $("<p></p>").text(fetch_api_attempt);
+  $(jq_api_div_data).append(user_para);
+}
+
+/**
+ *
+ * @param {boolean} isDog: A boolean variable to specify which API  call to make. If true, a call to the random dog API ("https://dog.ceo/api/breeds/image/random") is made. If false, a call to the random user API ("https://randomuser.me/api/") is made.
+ */
+function fetchAPIData(isDog) {
+  let api_url = "";
+  if (isDog) {
+    api_url = dog_api_url;
+  } else {
+    api_url = ruser_api_url;
+  }
+  $.get(dog_api_url, function (data) {
+    console.log(data);
+  }).fail(function () {
+    alert(fetch_api_err);
+  });
+}
+
+/** Add a click event listener to the "Fetch Doggie" button. When the
+ * user clicks the button, the associated function makes the API call.
+ * The user is informed of this action. If the action is successful,
+ * update the HTML page. If it's not successful, let the user.
+ */
+$(jq_submit_btn_dog).on("click", function () {
+  updateUserOnAttempt();
+  fetchAPIData(true);
+});
