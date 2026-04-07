e001e9d8bfcd6bdcb500c20c36367c7b3b69c612
David H. Watson <hiranwatson@gmail.com>
2026-04-07 06:47:57 -0400

    Created global constants to hold references as strings to the necessary classes and ids that will be used by jQuery

--- local/canadian-business-college/full-stack-web-application/Module5_JS_jQuery/assignments/4/code/js/assignment_four.js
+++ local/canadian-business-college/full-stack-web-application/Module5_JS_jQuery/assignments/4/code/js/assignment_four.js
@@ -1,3 +1,9 @@
-/** Global variables to help the API URLs */
+/** Global constants to hold the API URLs */
 const dog_api_url = "https://dog.ceo/api/breeds/image/random";
 const ruser_api_url = "https://randomuser.me/api/";
+
+/** Global constants to hold the necessary class and id names that will be referenced with jQuery */
+const jq_submit_btn_dog = "#submit-btn-dog";
+const jq_submit_btn_human = "#submit-btn-human";
+const jq_api_div_dog = "#api-div-dog";
+const jq_api_div_human = "#api-div-human";
