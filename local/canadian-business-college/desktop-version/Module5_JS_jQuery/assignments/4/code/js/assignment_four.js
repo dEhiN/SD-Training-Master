dc3ca5caf773800ab9ba68280041bd3c31ad74d9
David H. Watson <hiranwatson@gmail.com>
2026-04-07 07:25:56 -0400

    Minor changes (see full message for details)
    Adjusted the comment for the global variables in assignment_four.js.
    
    Also renamed the ids of the two div containers where the API data will be posted.

--- local/canadian-business-college/desktop-version/Module5_JS_jQuery/assignments/4/code/js/assignment_four.js
+++ local/canadian-business-college/desktop-version/Module5_JS_jQuery/assignments/4/code/js/assignment_four.js
@@ -1,8 +1,8 @@
-/** Global constants to hold the API URLs */
+/** Global constants to hold the API URLs. */
 const dog_api_url = "https://dog.ceo/api/breeds/image/random";
 const ruser_api_url = "https://randomuser.me/api/";
 
-/** Global constants to hold the necessary class and id names that will be referenced with jQuery */
+/** Global constants to hold the necessary class and id names that will be referenced with jQuery. */
 const jq_submit_btn_dog = "#submit-btn-dog";
 const jq_submit_btn_human = "#submit-btn-human";
 const jq_api_div_dog = "#api-div-dog";
