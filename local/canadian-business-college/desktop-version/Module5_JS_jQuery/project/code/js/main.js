5befafa8a26ebc18ded0979d5875c78afcf5afba
David H. Watson <hiranwatson@gmail.com>
2026-04-14 17:58:15 -0400

    Added import statements to the main.js files

--- local/canadian-business-college/desktop-version/Module5_JS_jQuery/project/code/js/main.js
+++ local/canadian-business-college/desktop-version/Module5_JS_jQuery/project/code/js/main.js
@@ -1,1 +1,5 @@
 // This JS file will function as the main JS that's linked to the HTML. The script element in the HTML will need to have the attribute 'type="module"'.
+
+/** Imports from the two API JS modules */
+import { } from './university_api';
+import { } from './weather_api';
