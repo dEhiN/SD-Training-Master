db8471656c8b0445c98f4b80a437cf10e51f8a11
David H. Watson <hiranwatson@gmail.com>
2026-04-15 05:03:55 -0400

    Set up functionality for the feature chooser section
    Added the logic to each of the API buttons for the feature chooser or "Pick Your API" section.
    
    The correct assignment section is displayed based upon the button clicked.

--- local/canadian-business-college/desktop-version/Module5_JS_jQuery/project/code/js/main.js
+++ local/canadian-business-college/desktop-version/Module5_JS_jQuery/project/code/js/main.js
@@ -1,6 +1,7 @@
 // This JS file will function as the main JS that's linked to the HTML. The script element in the HTML will need to have the attribute 'type="module"'.
 
 
+// MODULE IMPORTS
 /** 
  * Imports from the API JS modules.
  */
@@ -8,14 +9,77 @@ import { } from './weather_api.js';
 import { dogButtonListener } from './dog_api.js';
 import { universityButtonListener } from './university_api.js';
 
+
+// GLOBAL VARIABLES AND CONSTANTS
+/** 
+ * Global constants to hold HTML class and id references.
+ */
+let feature_containers_list = document.querySelectorAll(".feature-container");
+let feature_btn_weather = document.querySelector("#feature-btn-weather");
+let feature_btn_dog = document.querySelector("#feature-btn-dog");
+let feature_btn_university = document.querySelector("#feature-btn-university");
+
+
+// FUNCTIONS
+/**
+ * Function to hide all the feature containers
+ */
+function hideFeatureContainers() {
+    feature_containers_list.forEach(function (feature_container) {
+        feature_container.style.display = "none";
+    })
+}
+
+/**
+ * Function to check which button was pressed for the API buttons in the feature-chooser section. The appropriate assignment section is then shown.
+ * 
+ * @param {HTMLElement} button_element: Represents an HTMLElement that references a button
+ */
+function handleApiButton(button_element) {
+    // Hide all feature containers first
+    hideFeatureContainers();
+
+    // Check to see which button was pressed
+    if (button_element == feature_btn_weather) {
+        // Display the Weather Forecast section
+        feature_containers_list[0].style.display = "";
+    }
+    else if (button_element == feature_btn_dog) {
+        // Display the Doggie Image section
+        feature_containers_list[1].style.display = "";
+    }
+    else if (button_element == feature_btn_university) {
+        // Display the Higher Institutions section
+        feature_containers_list[2].style.display = "";
+    }
+}
+
+/**
+ * Function to set up the feature-chooser section
+ */
+function setupFeatureChooser() {
+    // Add click listeners to the API buttons
+    feature_btn_weather.addEventListener("click", function () {
+        handleApiButton(feature_btn_weather);
+    });
+    feature_btn_dog.addEventListener("click", function () {
+        handleApiButton(feature_btn_dog);
+    });
+    feature_btn_university.addEventListener("click", function () {
+        handleApiButton(feature_btn_university);
+    })
+}
+
+
+// MAIN CODE
 /** 
  * Call the imported functions. 
  */
 dogButtonListener();
 universityButtonListener();
 
-// let feature_containers_list = document.querySelectorAll(".feature-container");
-
-// feature_containers_list.forEach(function (feature_container) {
-//     feature_container.style.display = "none";
-// })
+/** 
+ * Call the main functions
+ */
+hideFeatureContainers();
+setupFeatureChooser();
