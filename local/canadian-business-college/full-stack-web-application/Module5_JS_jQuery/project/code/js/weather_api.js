846c3a5d249a989b1f32142afa30ae5bdfa0aa2d
David H. Watson <hiranwatson@gmail.com>
2026-04-15 16:33:20 -0400

    Several major changes
    Created an analogous clearData function as well as a validateUserInput function. Modelled the clearData function after the one in university_api.js.
    
    Also created global constants for each input field and for a couple of user alert messages that are used when the fields don't contain valid data.
    
    Finally, added the logic to the weatherButtonListener main function.

--- local/canadian-business-college/full-stack-web-application/Module5_JS_jQuery/project/code/js/weather_api.js
+++ local/canadian-business-college/full-stack-web-application/Module5_JS_jQuery/project/code/js/weather_api.js
@@ -45,16 +45,82 @@ const api_obj = {
  * Global constants to hold HTML class and id references.
  */
 const submit_btn_weather = document.querySelector("#submit-btn-weather");
+const input_weather_api_key = document.querySelector("#weather-api-key");
+const input_search_city = document.querySelector("#search-city");
+const input_forecast_days = document.querySelector("#forecast-days");
+
+/** Global constants to store user alert messages */
+const fill_all_fields = "Please fill in all 3 fields!";
+const invalid_number_days = "The number of days to forecast can only be between 1 and 5!";
+
 
 // FUNCTIONS
+/**
+ * Function to validate the input fields to make sure the user provided valid input.
+ * 
+ * @returns Boolean value specifying whether the user input was valid or not.
+ */
+function validateUserInput() {
+    /** Get the values of each input field. */
+    let api_key = input_weather_api_key.value;
+    let city = input_search_city.value;
+    let days = input_forecast_days.value;
 
+    /** Boolean to store whether the user input is fully valid or not. Assume false as default. */
+    let input_is_valid = false;
+
+    /** Check to make sure none of the fields are blank. If any are, let the user know. */
+    if (!api_key || !city || !days) {
+        alert(fill_all_fields);
+    }
+
+    /** Check the number of days inputted to make sure it's between 1 and 5. First, convert the days string to a number. */
+    let days_num = Number(days);
+    if (days_num < 1 || days_num > 5) {
+        alert(invalid_number_days)
+    }
+
+    /** If the input is not valid, clear the input fields plus any existing API results. If it is valid, update the inputIsValid variable.*/
+    if (!input_is_valid) {
+        clearData(true);
+    }
+    else {
+        input_is_valid = true;
+    }
+
+    /** The input is valid, so build the api_obj object. */
+    api_obj.parameters.key.param_value = api_key;
+    api_obj.parameters.city.param_value = city;
+    api_obj.parameters.days.param_value = days;
+
+    return input_is_valid;
+}
+
+/**
+ * Function to clear any existing API data from a previous call.
+ * 
+ * @param { boolean } all_data: A boolean to specify if all the existing data should be cleared or only some of it.
+ */
+export function clearData(all_data) {
+    // Check if the input fields need to be cleared
+    if (all_data) {
+        input_weather_api_key.value = "";
+        input_search_city.value = "";
+        input_forecast_days.value = "";
+    }
+}
 
 // MAIN CODE
 /** 
- * Add a click event listener to the "Fetch Doggie Image" button. When the user clicks the button, the function to fetch the actual API data is called.
+ * Add a click event listener to the "Fetch Daily Forecast" button. When the user clicks the button ...
  */
 export function weatherButtonListener() {
     submit_btn_weather.addEventListener("click", function () {
-        console.log("You pressed me!");
+        /** Validate the user input before proceeding. */
+        let valid_user_input = validateUserInput();
+
+        /** If the user input is valid, build the API url, fetch the API data, and process it. */
+        if (valid_user_input) {
+        }
     })
 };
