27b05e71bde2e83d053e66d70193d3e8ef4054d4
David H. Watson <hiranwatson@gmail.com>
2026-05-02 13:05:40 -0400

    Added the global variable app and set up the middleware

--- local/canadian-business-college/desktop-version/Module6_MERN_Stack/assignments/1/code/app.js
+++ local/canadian-business-college/desktop-version/Module6_MERN_Stack/assignments/1/code/app.js
@@ -3,3 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
+
+/** Global variables */
+const app = express();
+
+/** Middleware Setup */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
