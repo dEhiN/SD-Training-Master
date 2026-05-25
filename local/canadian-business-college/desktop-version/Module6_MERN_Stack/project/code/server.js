18cf870934f715cb6a8cb1bebe9ce61c151a9417
David H. Watson <hiranwatson@gmail.com>
2026-05-25 11:39:12 -0400

    Cleaned up server.js

--- local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/server.js
+++ local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/server.js
@@ -4,90 +4,28 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+
+/** Custom Module Imports */
+import connectToMongoDB from './config/database_config.js';
 
 
-/** Configure the dotenv module to add the variables in the .env file to the environment path. This is done at this point so the script specific global variables that rely on the environment variables can be initialized. The results are stored in a variable for easier use. */
-const dotConfigResults = dotenv.config().parsed;
+/** Configure the dotenv module to add the variables in the .env file to the environment path. This is done at this point so the script specific global variables that rely on the environment variables can be initialized. */
+dotenv.config();
 
 
-/** Script specific global variables */
-// Server listening port
+/** Variable to represent the server listening port. */
 const PORT = dotConfigResults.PORT || 4006;
-// Variables for the database connection
-const dbConnectionUri = dotConfigResults?.MONGODB_URI || process.env.MONGODB_URI;
-const dbUserName = dotConfigResults?.MONGODB_UNAME || process.env.MONGODB_UNAME;
-const dbUserPassword = dotConfigResults?.MONGODB_PWD || process.env.MONGODB_PWD;
-// Variables to assist with console and return messages
-const msgsObj = {
-    errors: {
-        SERVQUIT: "The server is quitting...",
-        ERRLEAD: "Please see below for the specific error:",
-        mongodb: {
-            DBFAIL: "The connection to the Mongo database could not be established.",
-            NODBURI: "The connection URI to the database is missing!",
-            NODBUSERNAME: "The database connection username is missing!",
-            NODBUSERPASSWORD: "The database connection password is missing!"
-        }
-    },
-    consoles: {
-        mongodb: {
-            DBATTEMPT: "Attempting connection to Mongo Atlas...",
-            DBCONNECTED: "Connection to the Mongo database was successfully established!"
-        }
-    },
-    misc: {
-        mongodb: {
-            DBUSERNAME: "<db_username>",
-            DBPASSWORD: "<db_password>"
-        }
-    }
-}
 
 
-/** Before continuing, check to make sure a connection to the database can be made. Start with inserting the db username and password into the connection uri if they all exist. If there are issues at any stage, throw an error. */
-async function connectToMongoDB() {
-    console.log(msgsObj.consoles.mongodb.DBATTEMPT)
-    try {
-        /** Check to make sure the db related variables have values. If not, throw an error */
-        if (!dbConnectionUri) {
-            throw new Error(msgsObj.errors.mongodb.NODBURI);
-        }
-        if (!dbUserName) {
-            throw new Error(msgsObj.errors.mongodb.NODBUSERNAME);
-        }
-        if (!dbUserPassword) {
-            throw new Error(msgsObj.errors.mongodb.NODBUSERPASSWORD);
-        }
-
-        /** Add the username and password to the connection URI */
-        let uri = dbConnectionUri;
-        uri = uri.replace(msgsObj.misc.mongodb.DBUSERNAME, dbUserName);
-        uri = uri.replace(msgsObj.misc.mongodb.DBPASSWORD, dbUserPassword);
-
-        /** Connect to the MongoDB */
-        await mongoose.connect(uri, {
-            autoIndex: false,
-            dbName: "kd-dd"
-        })
-        console.log(msgsObj.consoles.mongodb.DBCONNECTED);
-    }
-    catch (err) {
-        console.log(msgsObj.errors.mongodb.DBFAIL + "\n" + msgsObj.errors.ERRLEAD);
-        console.error(err);
-        console.log(msgsObj.errors.SERVQUIT);
-        process.exit(1);
-    }
-}
-connectToMongoDB();
+/** Connect to the database before continuing. */
+await connectToMongoDB();
 
 
-/** Module specific global variables:
- * - Create the Express app */
+/** Create the Express app */
 const app = express();
 
 
-/** Middleware setup:
+/** Set up global Express middleware:
  * - Sets up CORS
  * - Add the ability to handle complex form data through POST
  * - Add the ability to handle JSON data through POST
