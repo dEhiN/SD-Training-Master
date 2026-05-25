08b30cb27c38a8b676b5adfe2a3d011583bd756b
David H. Watson <hiranwatson@gmail.com>
2026-05-25 06:34:39 -0400

    Started coding in the Users model by adding a schema for the CreditCardDetails field

---
+++ local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/models/Users.js
@@ -0,0 +1,45 @@
+/** This acts as the Mongoose model for a User object. The schema defined here should correspond to the definition found in the local file "kd-dd_user.schema.json". Any and all database interaction regarding users will be handled by this model. */
+
+/** Named Module Imports */
+import mongoose from "mongoose";
+
+/** Start with the child schemas */
+const ccDetailsSchema = new mongoose.Schema({
+    CardId: {
+        type: String,
+        required: true
+    },
+    IsDefault: {
+        type: Boolean,
+        required: true
+    },
+    CC_Type: {
+        type: String,
+        enum: ["VISA", "MasterCard"],
+        required: true
+    },
+    CC_Name: {
+        type: String,
+        required: true
+    },
+    CC_Number: {
+        type: String,
+        minLength: 15,
+        maxLength: 16,
+        match: /^(4\d{15}|5[1-5]\d{14}|2(22[1-9]|2[3-9]\d|[3-6]\d{2}|7[0-1]\d|720)\d{12})$/,
+        required: true
+    },
+    "CC_ExpMonth": {
+        type: String,
+        match: /^(0[1-9]|1[0-2])$/,
+        required: true
+    },
+    "CC_ExpYear": {
+        type: String,
+        match: /^(\d{4})$/,
+        required: true
+    }
+}, {
+    _id: false,
+    strict: "throw"
+})
