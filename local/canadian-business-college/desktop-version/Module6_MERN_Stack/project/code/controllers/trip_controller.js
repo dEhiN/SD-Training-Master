8215d3e80fc9f7a8ce1de4ff8cf358a4a51b8186
David H. Watson <hiranwatson@gmail.com>
2026-05-26 15:32:03 -0400

    Added top-of-file comments to the two controller files

--- local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/controllers/trip_controller.js
+++ local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/controllers/trip_controller.js
@@ -1,2 +1,4 @@
+/** This acts as the controller for Trip endpoints. The functions attached to each Trip endpoint will be written here. These functions will interact with a global instance of the Trip model and use that to interact with the Mongo database. */
+
 export const calculateFare = () => { };
 export const bookTrip = () => { };
--- local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/controllers/user_controller.js
+++ local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/controllers/user_controller.js
@@ -1,3 +1,7 @@
+/** This acts as the controller for User endpoints. The functions attached to each User endpoint will be written here. These functions will interact with a global instance of the User model and use that to interact with the Mongo database. */
+
+
+
 export const getUserAccount = () => { };
 export const getUserTrips = () => { };
 export const filterUserTrips = () => { };
