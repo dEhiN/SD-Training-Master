7575136643cd8ce2368fa49628fcc4336bfdb26d
David H. Watson <hiranwatson@gmail.com>
2026-05-26 16:00:23 -0400

    Fleshed out the top-of-file comments, added the model imports and added the req/res parameters to the route functions
    Added a list to the top-of-file comments of each API endpoint and the associated route function.
    
    Added the correct Mongoose model import statement.
    
    Added the req/res parameters to all functions.

--- local/canadian-business-college/full-stack-web-application/Module6_MERN_Stack/project/code/controllers/user_controller.js
+++ local/canadian-business-college/full-stack-web-application/Module6_MERN_Stack/project/code/controllers/user_controller.js
@@ -1,9 +1,24 @@
-/** This acts as the controller for User endpoints. The functions attached to each User endpoint will be written here. These functions will interact with a global instance of the User model and use that to interact with the Mongo database. */
+/** This acts as the controller for User endpoints. The functions attached to each User endpoint will be written here. These functions will interact with a global instance of the User model and use that to interact with the Mongo database. The API endpoints and their associated functions:
+ * 
+ * /profile = getUserAccount()
+ * /trips = getUserTrips()
+ * /trips/filter = filterUserTrips()
+ * /create-account = createUserAccount()
+ * /login = loginUser()
+*/
 
 
+/** Mongoose model import */
+import User from "../models/user_model.js";
 
-export const getUserAccount = () => { };
-export const getUserTrips = () => { };
-export const filterUserTrips = () => { };
-export const createAccount = () => { };
-export const loginUser = () => { };
+
+/** Route functions */
+export const getUserAccount = (req, res) => { };
+
+export const getUserTrips = (req, res) => { };
+
+export const filterUserTrips = (req, res) => { };
+
+export const createUserAccount = (req, res) => { };
+
+export const loginUser = (req, res) => { };
