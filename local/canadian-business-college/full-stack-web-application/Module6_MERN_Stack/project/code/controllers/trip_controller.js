4ee85894a55f1ce624f60d93a5930c060940f0aa
David H. Watson <hiranwatson@gmail.com>
2026-05-26 17:07:05 -0400

    Added some return code to bookTrip, fixed an incorrect model import, and added validation to /fare-estimate
    Realized that the trip data sent to /fare-estimate should be validated as well.

--- local/canadian-business-college/full-stack-web-application/Module6_MERN_Stack/project/code/controllers/trip_controller.js
+++ local/canadian-business-college/full-stack-web-application/Module6_MERN_Stack/project/code/controllers/trip_controller.js
@@ -6,20 +6,31 @@
 
 
 /** Mongoose model import */
-import User from "../models/user_model.js";
+import Trip from "../models/trip_model.js";
 
 
 /** Route functions */
 export const calculateFare = (req, res) => { };
 
 export const bookTrip = async (req, res) => {
+    console.log("Hello from inside the function bookTrip!");
+
     try {
         // Create a new Trip document using the passed in POST data since it's already been validated
         const newTrip = new Trip(req.body);
 
-        console.log(newTrip);
+        console.log("A new Trip document was created! Attempting to save.");
+
+        return res.status(200).json({
+            payload: newTrip
+        })
+
     }
     catch (err) {
-
+        return res.status(500).json({
+            status: "fail",
+            message: "Something went wrong! Please see the error(s) below:",
+            errors: err
+        })
     }
 };
