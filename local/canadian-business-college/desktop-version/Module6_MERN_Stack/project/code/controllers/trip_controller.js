c9ed6beaa89f868521dd18e57273c2d23c11024c
David H. Watson <hiranwatson@gmail.com>
2026-05-26 16:00:23 -0400

    Fleshed out the top-of-file comments, added the model imports and added the req/res parameters to the route functions
    Added a list to the top-of-file comments of each API endpoint and the associated route function.
    
    Added the correct Mongoose model import statement.
    
    Added the req/res parameters to all functions.

--- local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/controllers/trip_controller.js
+++ local/canadian-business-college/desktop-version/Module6_MERN_Stack/project/code/controllers/trip_controller.js
@@ -1,4 +1,15 @@
-/** This acts as the controller for Trip endpoints. The functions attached to each Trip endpoint will be written here. These functions will interact with a global instance of the Trip model and use that to interact with the Mongo database. */
+/** This acts as the controller for Trip endpoints. The functions attached to each Trip endpoint will be written here. These functions will interact with a global instance of the Trip model and use that to interact with the Mongo database. The API endpoints and their associated functions:
+ * 
+ * /fare-estimate = calculateFare()
+ * /booking = bookTrip()
+*/
 
-export const calculateFare = () => { };
-export const bookTrip = () => { };
+
+/** Mongoose model import */
+import User from "../models/user_model.js";
+
+
+/** Route functions */
+export const calculateFare = (req, res) => { };
+
+export const bookTrip = (req, res) => { };
