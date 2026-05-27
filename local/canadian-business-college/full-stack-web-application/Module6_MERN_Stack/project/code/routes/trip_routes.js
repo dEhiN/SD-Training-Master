7b053f440f9e028ed93850f569f463cc81a13a2d
David H. Watson <hiranwatson@gmail.com>
2026-05-27 02:44:18 -0400

    Renamed the /api/trips/booking endpoint to /api/trips/book-trip

--- local/canadian-business-college/full-stack-web-application/Module6_MERN_Stack/project/code/routes/trip_routes.js
+++ local/canadian-business-college/full-stack-web-application/Module6_MERN_Stack/project/code/routes/trip_routes.js
@@ -1,9 +1,9 @@
 /** This serves as the router for Trip endpoints. The following endpoints exist:
  * 
  * /fare-estimate = to calculate and return a fare estimate for a proposed trip
- * /booking = to book a trip
+ * /book-trip = to book a trip
  * 
- * Last updated: 2026-05-26
+ * Last updated: 2026-05-27
 */
 
 
@@ -21,6 +21,6 @@ const tripRouter = express.Router();
 
 /** Set up the routes. These will be based on '/' because server.js will define the API trip endpoint for these routes. */
 tripRouter.post("/fare-estimate", validateTrip, calculateFare);
-tripRouter.post("/booking", validateTrip, bookTrip);
+tripRouter.post("/book-trip", validateTrip, bookTrip);
 
 export default tripRouter;
