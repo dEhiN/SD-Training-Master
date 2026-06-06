4053aa46d1ce7c49f4900ee2d874de6ba20d26eb
Hiran Watson <hiranwatson@gmail.com>
2026-06-06 19:59:55 -0400

    Added some rules to eslint config

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/eslint.config.js
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/eslint.config.js
@@ -1,25 +1,32 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier/flat';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    },
-    plugins: { prettier: prettierPlugin },
-    rules: {},
-  },
+	{
+		files: ["**/*.{js,jsx}"],
+		extends: [
+			js.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: { ecmaFeatures: { jsx: true } },
+		},
+		plugins: { prettier: prettierPlugin },
+		rules: {
+			indent: ["error", 4],
+			semi: ["error", "always"],
+			quotes: "off",
+			"keyword-spacing": ["error", { before: true, after: true }],
+			"linebreak-style": ["error", "unix"],
+			"spaced-comment": ["error", "always"],
+			"no-var": "error",
+		},
+	},
 ]);
