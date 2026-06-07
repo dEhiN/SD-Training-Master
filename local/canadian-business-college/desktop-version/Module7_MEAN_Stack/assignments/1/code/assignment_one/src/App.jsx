89f4bb59a6670caad28368e3a79b24d5f7c75486
Hiran Watson <hiranwatson@gmail.com>
2026-06-07 09:40:43 -0400

    Added the browser router, navbar, and route components to App.jsx

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/App.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/App.jsx
@@ -1,7 +1,26 @@
-// import "./App.css";
+/** This is the main React app */
 
-function App () {
-	return <></>;
+/** React-specific component imports */
+import { BrowserRouter, Routes, Route } from "react-router";
+
+/** App-specific (custom) component imports */
+import NavBar from "./components/NavBar";
+
+/** This is the main App component function. It is configured to just set up the routes and the navigation bar.
+ *
+ * @returns {JSX.Element} The full app complete with navigation bar, the browser router and the page routes configured.
+ */
+function App() {
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Routes>
+				<Route path="/" element="" />
+				<Route path="/userinfoform" element="" />
+				<Route path="/userlist" element="" />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
