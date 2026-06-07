00ebb5fae77923f07aed80f3a4e4311121cda382
David H. Watson <hiranwatson@gmail.com>
2026-06-07 16:45:15 -0400

    Set up css styling for the page headers

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/HeadingLevelOne.jsx
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/HeadingLevelOne.jsx
@@ -1,5 +1,8 @@
 /** This component will act as an h1 element but will allow for dynamic inner text content. The purpose is so that all h1 elements can be changed simultaneously regarding things like CSS classes, rather than needing to emulate one change across all 3 pages. */
 
+/** CSS import */
+import "./HeadingLevelOne.css";
+
 /**
  * This component function returns an h1 element with the inner text content dynamically added using props.
  *
@@ -8,7 +11,7 @@
  * @return {JSX.Element} The rendered h1 element
  */
 function HeadingLevelOne(props) {
-	return <h1 className="fonts-google iosevka-charon-bold">{props.text}</h1>;
+	return <h1 className="fonts-google iosevka-charon-bold main-heading">{props.text}</h1>;
 }
 
 export default HeadingLevelOne;
