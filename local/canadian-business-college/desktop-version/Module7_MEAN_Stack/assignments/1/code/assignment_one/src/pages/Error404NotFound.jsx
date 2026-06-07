9dc427684ac0316c441d467cccc2d3fec5f51ff5
David H. Watson <hiranwatson@gmail.com>
2026-06-07 13:48:54 -0400

    Updated the p elements to use a GF (Google font)

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/Error404NotFound.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/Error404NotFound.jsx
@@ -15,11 +15,13 @@ function Error404NotFound() {
 	return (
 		<>
 			<HeadingLevelOne text="404: Page Not Found" />
-			<p>
+			<p className="fonts-google iosevka-charon-regular">
 				Uh-oh, the dreaded <span className="html-error-code">404</span> error!
 			</p>
-			<p>You must have wandered off the yellow brick road into the forest...</p>
-			<p>
+			<p className="fonts-google iosevka-charon-regular">
+				You must have wandered off the yellow brick road into the forest...
+			</p>
+			<p className="fonts-google iosevka-charon-regular">
 				Please use the navigation links at the top to make your way safely back to the
 				yellow brick road!
 			</p>
