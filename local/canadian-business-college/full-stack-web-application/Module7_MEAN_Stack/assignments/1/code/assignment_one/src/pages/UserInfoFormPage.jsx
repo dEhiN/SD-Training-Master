b995e48c46092167ba0d0506efba7ba364c2e3f8
David H. Watson <hiranwatson@gmail.com>
2026-06-07 13:33:16 -0400

    Updated all the pages to use the new HeadingLevelOne component

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/UserInfoFormPage.jsx
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/UserInfoFormPage.jsx
@@ -1,12 +1,15 @@
 /** This component will act as the User Information Form page. */
 
+/** Custom component imports */
+import HeadingLevelOne from "../components/HeadingLevelOne";
+
 /**
  * This component function creates the User Information form page. That is, a page with a form for users to fill out. The data is then validated and sent to an Express server for storage in a database. This component acts as the counterpart to the UserListPage component.
  *
  * @return {JSX.Element} The full rendered User Information page.
  */
 function UserInfoFormPage() {
-	return <h1>User Information</h1>;
+	return <HeadingLevelOne text="User Information" />;
 }
 
 export default UserInfoFormPage;
