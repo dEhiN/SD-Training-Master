5c04ec2d284acff7a5a4ee40875228349f36fea5
David H. Watson <hiranwatson@gmail.com>
2026-06-07 13:22:01 -0400

    Surrounded the p element in a div for future styling and added 3 more sentences; each sentence shows off one of the 4 Google Fonts currently being considered for the project

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/WelcomeCard.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/WelcomeCard.jsx
@@ -2,9 +2,24 @@
 
 function WelcomeCard() {
 	return (
-		<>
-			<p>Welcome to my first React app!</p>
-		</>
+		<div>
+			<p className="fonts-google iosevka-charon-regular">
+				Welcome to my first React app! This sentence using Ioveska Charon regular as the
+				font. Do you like it?
+			</p>
+			<p className="fonts-google iosevka-charon-bold">
+				Welcome to my first React app! This sentence using Ioveska Charon bold as the font.
+				Do you like it?
+			</p>
+			<p className="fonts-google playwrite-au-vic-lite">
+				Welcome to my first React app! This sentence using Playwrite AU Vic with weight 159
+				as the font. Do you like it?
+			</p>
+			<p className="fonts-google playwrite-au-vic-guides-regular">
+				Welcome to my first React app! This sentence using Playwrite AU Vic Guides as the
+				font. Do you like it?
+			</p>
+		</div>
 	);
 }
 
