7525e62f2894082fb9de14dab398df9df962638e
David H. Watson <hiranwatson@gmail.com>
2026-06-07 10:59:07 -0400

    Adjusted some comment styling

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/NavBar.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/NavBar.jsx
@@ -1,11 +1,12 @@
-/** This component will act as the top navigation section for every page */
+/** This component will act as the top navigation section for every page. */
 
 /** Use the NavButtonLink component */
 import NavButtonLink from "./NavButtonLink";
 
-/** This component function creates a navigation bar. The bar itself is a div component with a nav component nested inside. This allows for both styling and HTML semantics.
+/**
+ * This component function creates a navigation bar. The bar itself is a div component with a nav component nested inside. This allows for both styling and HTML semantics.
  *
- * @returns {JSX.Element} The rendered navigation bar complete with all links it should display
+ * @returns {JSX.Element} The rendered navigation bar complete with all links it should display.
  */
 function NavBar() {
 	return (
