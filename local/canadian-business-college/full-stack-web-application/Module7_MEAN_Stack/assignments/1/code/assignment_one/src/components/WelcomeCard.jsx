93993eec9e6f4fdc8ae19fdc68163f95abb76898
David H. Watson <hiranwatson@gmail.com>
2026-06-07 12:34:22 -0400

    Added the WelcomeCard to the HomePage and a simple sentence for now

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/WelcomeCard.jsx
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/WelcomeCard.jsx
@@ -1,1 +1,11 @@
 /** This component will act as a welcome card that can be featured in the hero section of the main page. */
+
+function WelcomeCard() {
+	return (
+		<>
+			<p>Welcome to my first React app!</p>
+		</>
+	);
+}
+
+export default WelcomeCard;
--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
@@ -1,12 +1,20 @@
 /** This component will act as the Home page. */
 
+/** Custom component imports */
+import WelcomeCard from "../components/WelcomeCard";
+
 /**
  * This component function creates the Home page
  *
  * @return {JSX.Element} The full rendered Home or main page.
  */
 function HomePage() {
-	return <h1>Home</h1>;
+	return (
+		<>
+			<h1>Home</h1>
+			<WelcomeCard />
+		</>
+	);
 }
 
 export default HomePage;
