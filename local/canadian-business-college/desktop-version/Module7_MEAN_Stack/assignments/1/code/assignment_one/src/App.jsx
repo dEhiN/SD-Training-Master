038b12f3416c8c29266ae772805f5f2483593404
David H. Watson <hiranwatson@gmail.com>
2026-06-07 12:27:00 -0400

    Modified App.jsx to use the MainSection component and pass in the correct page component name

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/App.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/App.jsx
@@ -6,24 +6,29 @@ import { BrowserRouter, Routes, Route } from "react-router";
 /** App-specific (custom) component imports */
 import NavSection from "./components/NavSection";
 import MainSection from "./components/MainSection";
-import HomePage from "./pages/HomePage";
-import UserInfoFormPage from "./pages/UserInfoFormPage";
-import UserListPage from "./pages/UserListPage";
-import Error404NotFound from "./pages/Error404NotFound";
 
 /** This is the main App component function. It is configured to just set up the routes and the navigation bar.
  *
  * @returns {JSX.Element} The full app complete with navigation bar, the browser router and the page routes configured.
  */
 function App() {
+	// An array to keep track of the pages and their component names
+	const pages = ["HomePage", "UserInfoFormPage", "UserListPage", "Error404NotFound"];
+
 	return (
 		<BrowserRouter>
 			<NavSection />
 			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/userinfoform" element={<UserInfoFormPage />} />
-				<Route path="/userlist" element={<UserListPage />} />
-				<Route path="*" element={<Error404NotFound />} />
+				<Route path="/" element={<MainSection pageComponentName={pages[0]} />} />
+				<Route
+					path="/userinfoform"
+					element={<MainSection pageComponentName={pages[1]} />}
+				/>
+				<Route path="/userlist" element={<MainSection pageComponentName={pages[2]} />} />
+				<Route
+					path="*"
+					element={<MainSection pageComponentName={pages[pages.length - 1]} />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
