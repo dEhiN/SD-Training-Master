bbfd88bb3eed78b3b02e79015ed5e135d8cb5006
David H. Watson <hiranwatson@gmail.com>
2026-06-07 10:58:58 -0400

    Started coding in the HomePage component

---
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+/** This component will act as the Home page. */
+
+/**
+ * This component function creates the Home page
+ *
+ * @return {JSX.Element} The full rendered Home or main page.
+ */
+function HomePage() {
+	return <h1>Home</h1>;
+}
+
+export default HomePage;
