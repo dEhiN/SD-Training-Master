a9ab3144c1cafa6c513cfc24d77adc4286ebc465
David H. Watson <hiranwatson@gmail.com>
2026-06-07 11:09:36 -0400

    Renamed UserInfoForm and UserList to UserInfoFormPage and UserListPage respectively, and also coded each file in using the same code from HomePage - this allows for all three pages to have some data to display

---
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/UserInfoFormPage.jsx
@@ -0,0 +1,12 @@
+/** This component will act as the User Information Form page. */
+
+/**
+ * This component function creates the User Information form page. That is, a page with a form for users to fill out. The data is then validated and sent to an Express server for storage in a database. This component acts as the counterpart to the UserListPage component.
+ *
+ * @return {JSX.Element} The full rendered User Information page.
+ */
+function UserInfoFormPage() {
+	return <h1>User Information</h1>;
+}
+
+export default UserInfoFormPage;
