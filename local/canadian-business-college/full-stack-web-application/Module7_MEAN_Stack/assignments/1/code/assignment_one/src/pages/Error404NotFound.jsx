a28fa0577c1d3e59e7eeda83a6d9a8423c37e427
David H. Watson <hiranwatson@gmail.com>
2026-06-07 11:38:47 -0400

    Created a 404 page

---
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/Error404NotFound.jsx
@@ -0,0 +1,24 @@
+/** This component will act as default page for any 404 errors. */
+
+/**
+ * This component function creates a 404 error page to display.
+ *
+ * @return {JSX.Element} The full rendered error page.
+ */
+function Error404NotFound() {
+	return (
+		<>
+			<h1>404: Page Not Found</h1>
+			<p>
+				Uh-oh, the dreaded <span className="html-error-code">404</span> error!
+			</p>
+			<p>You must have wandered off the yellow brick road into the forest...</p>
+			<p>
+				Please use the navigation links at the top to make your way safely back to the
+				yellow brick road!
+			</p>
+		</>
+	);
+}
+
+export default Error404NotFound;
