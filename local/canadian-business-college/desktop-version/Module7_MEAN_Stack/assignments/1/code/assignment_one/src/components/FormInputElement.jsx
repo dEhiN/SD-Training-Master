8c2a1ce3be22909a9e3c180e7e7e06ceed4272ee
David H. Watson <hiranwatson@gmail.com>
2026-06-07 21:54:31 -0400

    Updated the FormInputElement to add a label

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/FormInputElement.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/components/FormInputElement.jsx
@@ -1,16 +1,28 @@
 /** This component will act as a form input element. */
 
 /**
- * This component function will create a form input element using the props values passed in. It can only create and handle form input elements, not any other type of form elements.
+ * This component function will create a form input element using the props values passed in. It will also add the element label if the same props are passed in. It can only create and handle form input elements, not any other type of form elements.
  *
  * @param {Object} props - The component props.
- * @param {string} props.type - The type of input field to create.
- * @param {string} props.name- The name for the input field.
- * @param {string} props.placeholder - The placeholder text (if any) to specify.
+ * @param {string} props.inputType - The type of input field to create.
+ * @param {string} props.inputName- The name for the input field.
+ * @param {string} props.inputPlaceholder - The placeholder text (if any) to specify for the input field.
+ * @param {string} props.id - The id for the input field. This will also be used as the "for" attribute of the associated label element.
+ * @param {string} props.labelText - The label text to display.
  * @returns {JSX.Element} The rendered input form element.
  */
 function FormInputElement(props) {
-	return <input type={props.type} name={props.name} placeholder={props.placeholder} />;
+	return (
+		<>
+			<label htmlFor={props.id}>{props.labelText}</label>
+			<input
+				type={props.inputType}
+				name={props.inputName}
+				id={props.id}
+				placeholder={props.inputPlaceholder}
+			/>
+		</>
+	);
 }
 
 export default FormInputElement;
