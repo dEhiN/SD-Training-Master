d6cc9ddc4d42e9f64de9042784459558683265fb
Hiran Watson <hiranwatson@gmail.com>
2026-06-08 06:14:32 -0400

    Made significant additions to the HomePage by adding more WelcomeCards

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
@@ -18,9 +18,13 @@ function HomePage() {
 			<HeadingLevelOne text="Home" />
 			<WelcomeCard wcVersion={1}>
 				<p className="fonts-google caveat-semibold font-size-bg">
-					Welcome to my first React app! It is a 3 page site that functions like a Single
-					Page Application. The 3 pages correspond to the nav links at the top of the
-					page:
+					Welcome to my first React app!
+				</p>
+				<p className="fonts-google caveat-medium font-size-md">
+					It is a 3 page site that functions like a Single Page Application.
+				</p>
+				<p className="fonts-google caveat-medium font-size-md">
+					The 3 pages correspond to the nav links at the top of the page:
 				</p>
 				<ul>
 					<li className="fonts-google caveat-medium font-size-md">Home</li>
@@ -29,9 +33,47 @@ function HomePage() {
 				</ul>
 			</WelcomeCard>
 			<WelcomeCard wcVersion={2}>
-				<p className="fonts-google caveat-medium font-size-bg">
-					This app collects and saves user information through a form and then will show
-					all saved information in a list.
+				<p className="fonts-google caveat-semibold font-size-bg">
+					What does this app do? Well, this app does two things:
+				</p>
+				<ol>
+					<li className="fonts-google caveat-medium font-size-md">
+						Collect user information through a form and save that information.
+					</li>
+					<li className="fonts-google caveat-medium font-size-md">
+						Show all saved user information in the form of a list.
+					</li>
+				</ol>
+			</WelcomeCard>
+			<WelcomeCard wcVersion={1}>
+				<p className="fonts-google caveat-semibold font-size-bg">
+					What is a Single Page Application?
+				</p>
+				<p className="fonts-google caveat-medium font-size-md">
+					A Single Page Application (also called SPA) is a website that functions like an
+					application, or program, and only has 1 page to the site.
+				</p>
+				<p className="fonts-google caveat-medium font-size-md">
+					A good example of an SPA site is Facebook. In fact, any social media site is an
+					SPA. While there are buttons to click and there's interaction with the site, the
+					entire site or page never reloads or refreshes.
+				</p>
+			</WelcomeCard>
+			<WelcomeCard wcVersion={2}>
+				<p className="fonts-google caveat-semibold font-size-bg">
+					Aren't all websites the same? Actually, no.
+				</p>
+				<p className="fonts-google caveat-medium font-size-md">
+					When the Internet was first created, and when websites were first developed,
+					they only existed to show or serve information. The information on a site stayed
+					the same, so you could visit the site every day for a month and you would read
+					the same text.
+				</p>
+				<p className="fonts-google caveat-medium font-size-md">
+					As the Internet, and websites, developed, the information served evolved to
+					become dynamic. That is, the information could change every time the site was
+					viewed. Eventually, websites evolved more to become interactive in the same way
+					as a mobile app or laptop software program.
 				</p>
 			</WelcomeCard>
 		</>
