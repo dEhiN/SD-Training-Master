abfec82ca8c6c93666d7a3ca5bd64f205b2deb1a
David H. Watson <hiranwatson@gmail.com>
2026-06-07 21:30:08 -0400

    Refactored the way WelcomeCard works for that the component takes in props specifying which card version to generate and the children to add to the card; this means that the caller component MUST specify the props value expected and give children if the card is expected have content.

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/HomePage.jsx
@@ -13,7 +13,18 @@ function HomePage() {
 	return (
 		<>
 			<HeadingLevelOne text="Home" />
-			<WelcomeCard />
+			<WelcomeCard wcVersion={1}>
+				<p className="fonts-google caveat-semibold wc-size-big">
+					Welcome to my first React app! It is a 3 page site that functions like a Single
+					Page Application. The 3 pages correspond to the nav links at the top of the
+					page:
+				</p>
+				<ul>
+					<li className="fonts-google caveat-medium wc-size-md">Home</li>
+					<li className="fonts-google caveat-medium wc-size-md">User Information</li>
+					<li className="fonts-google caveat-medium wc-size-md">User List</li>
+				</ul>
+			</WelcomeCard>
 		</>
 	);
 }
