1b637ecfae57faad3c49b3e83c8ec34e931ad5aa
David H. Watson <hiranwatson@gmail.com>
2026-06-09 16:25:42 -0400

    Created the UserListPage component and added some styling

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/UserListPage.jsx
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/src/pages/UserListPage.jsx
@@ -1,7 +1,15 @@
 /** This component will act as the User List page. */
 
+/** CSS import */
+import "./UserListPage.css";
+
+/** React imports */
+import { useState } from "react";
+
 /** Custom component imports */
 import HeadingLevelOne from "../components/HeadingLevelOne";
+import WelcomeCard from "../components/WelcomeCard";
+import FormInputElement from "../components/FormInputElement";
 
 /**
  * This component function creates the User List page. That is, a page with a user's information shown in a list format. The data is fetched from an Express server that takes it from a database. This component acts as the counterpart to the UserInfoFormPage component.
@@ -9,7 +17,67 @@ import HeadingLevelOne from "../components/HeadingLevelOne";
  * @return {JSX.Element} The full rendered User Information page.
  */
 function UserListPage() {
-	return <HeadingLevelOne text="User List" />;
+	// An array to store the user data retrieved from the database
+	const [users, setUsers] = useState([]);
+
+	// Simple boolean variable for the state of the "Fetch all users" button
+	const [isButtonPressed, setIsButtonPressed] = useState(false);
+
+	// API GET route to use for sending the user data
+	const apiGetRoute = "/api/get-user";
+
+	// Variable to count the number of users
+	let userCount = 0;
+
+	/**
+	 * This function retrieves the actual user data from the server GET route. It then calls the set state function to add that data to the users array
+	 *
+	 */
+	const getUsers = async () => {
+		let apiResponse = await fetch(apiGetRoute);
+		let userData = await apiResponse.json();
+		setUsers(userData.results);
+		buttonPressed();
+	};
+
+	const buttonPressed = () => {
+		setIsButtonPressed(!isButtonPressed);
+	};
+
+	return (
+		<>
+			<HeadingLevelOne text="Users List" />
+			{!isButtonPressed && (
+				<WelcomeCard wcVersion={3}>
+					<FormInputElement
+						inputType="submit"
+						inputValue={"Fetch all users! "}
+						submitFunction={getUsers}
+					/>
+				</WelcomeCard>
+			)}
+			{users &&
+				users.map(
+					(user) => (
+						userCount++,
+						(
+							<WelcomeCard wcVersion={2}>
+								<p className="user-list-header">User {userCount}</p>
+								<ul className="user-list-list">
+									<li>
+										Name: {user.firstName} {user.lastName}
+									</li>
+									<li>Age: {user.age}</li>
+									<li>Company: {user.company}</li>
+									<li>Position: {user.role}</li>
+									<li>Email: {user.email}</li>
+								</ul>
+							</WelcomeCard>
+						)
+					)
+				)}
+		</>
+	);
 }
 
 export default UserListPage;
