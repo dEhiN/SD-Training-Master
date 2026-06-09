b1df84672c83e8efa09aa47efe20dfe27b59e60c
David H. Watson <hiranwatson@gmail.com>
2026-06-09 07:38:54 -0400

    Adjusted the eslint config settings

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/eslint.config.js
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier/flat";
 
@@ -29,4 +29,5 @@ export default defineConfig([
 			"no-unused-vars": "off",
 		},
 	},
+	prettierConfig,
 ]);
--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/other-es_config.cjs
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/assignments/1/code/assignment_one/other-es_config.cjs
@@ -8,17 +8,9 @@ module.exports = defineConfig([
 		rules: {
 			"no-console": "off", // Allow console statements for debugging
 			"func-names": "off", // Allow unnamed functions for flexibility
-			"spaced-comment": ["error", "always"], // Enforce spacing after comment markers
-			"no-inline-comments": "off", // Allow inline comments for clarity
 			"multiline-comment-style": "off", // Allow any style of multiline comments
 			// Prettier controls quote style; disable ESLint's quote rule
-			quotes: "off", // Disable ESLint's quote rule
-			semi: ["error", "always"], // Enforce semicolons at the end of statements
-			"no-unused-vars": ["warn"], // Warn about unused variables to keep code clean
-			indent: ["error", 2], // Enforce consistent indentation of 2 spaces
-			"no-var": "error", // Disallow var, use let or const instead
 			"linebreak-style": ["error", "unix"], // Enforce Unix linebreaks for cross-platform consistency
-			"keyword-spacing": ["error", { before: true, after: true }], // Enforce spacing around keywords for readability
 			"no-restricted-globals": "off", // Allow all global variables
 			"no-alert": "off", // Allow alert for simple debugging purposes
 			"no-plusplus": "off", // Allow ++ operator for incrementing values
