a7ae4e717cebd0be44ec50948f695bb2a62b1102
David H. Watson <hiranwatson@gmail.com>
2026-06-23 23:57:28 -0400

    Applied the new formatting to app.ts

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/in-class/code/angular-beginner-assignment/src/app/app.ts
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/in-class/code/angular-beginner-assignment/src/app/app.ts
@@ -1,14 +1,14 @@
-import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-import { DataForm } from './data-form/data-form';
-import { DataList } from './data-list/data-list';
+import { Component, signal } from "@angular/core";
+// import { RouterOutlet } from "@angular/router";
+import { DataForm } from "./data-form/data-form";
+import { DataList } from "./data-list/data-list";
 
 @Component({
-  selector: 'app-root',
-  imports: [DataForm, DataList],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+	selector: "app-root",
+	imports: [DataForm, DataList],
+	templateUrl: "./app.html",
+	styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('angular-beginner-assignment');
+	protected readonly title = signal("angular-beginner-assignment");
 }
