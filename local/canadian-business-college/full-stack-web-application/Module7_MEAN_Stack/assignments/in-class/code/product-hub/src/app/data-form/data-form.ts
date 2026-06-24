eebafb57cd5e1f1bda51348dfcf0908ab7187a6f
David H. Watson <hiranwatson@gmail.com>
2026-06-23 22:49:52 -0400

    Created two new components for ICA 5 part 1 - Data-Form and Data-List

---
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/in-class/code/product-hub/src/app/data-form/data-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-form',
+  imports: [],
+  templateUrl: './data-form.html',
+  styleUrl: './data-form.css',
+})
+export class DataForm {}
