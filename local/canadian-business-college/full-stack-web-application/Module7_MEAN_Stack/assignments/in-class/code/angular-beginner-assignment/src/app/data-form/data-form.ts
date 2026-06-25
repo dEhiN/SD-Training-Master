2341f44142c25bd8a82c6407d1bd0e34340127e5
David H. Watson <hiranwatson@gmail.com>
2026-06-24 23:14:44 -0400

    Completed parts 1-2 of ICA 7: 1) Created an object in the Data Form component to represent the form data and a method to handle the submit button click; 2) Added the form to the component template contained within a section and using ngModel to bind each form element (input + textarea) to the component class object

--- local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/in-class/code/angular-beginner-assignment/src/app/data-form/data-form.ts
+++ local/canadian-business-college/full-stack-web-application/Module7_MEAN_Stack/assignments/in-class/code/angular-beginner-assignment/src/app/data-form/data-form.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-data-form',
-  imports: [],
-  templateUrl: './data-form.html',
-  styleUrl: './data-form.css',
+	selector: "app-data-form",
+	imports: [FormsModule],
+	templateUrl: "./data-form.html",
+	styleUrl: "./data-form.css",
 })
-export class DataForm {}
+export class DataForm {
+	// Form model structure matching backend properties
+	formData = {
+		title: "",
+		body: "",
+	};
+
+	onSubmit() {
+		console.log("Form State Captured:", this.formData);
+		alert(`Success! Local data parsed:\nTitle: ${this.formData.title}`);
+	}
+}
