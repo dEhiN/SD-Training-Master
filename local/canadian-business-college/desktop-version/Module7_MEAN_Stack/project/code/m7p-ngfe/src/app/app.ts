ab86f0b5ec7fe5d32c993e29ab25b9357f7742c2
Hiran Watson <hiranwatson@gmail.com>
2026-07-11 18:12:45 -0400

    Cleared the default content for app.html and renamed the page title in app.ts

--- local/canadian-business-college/desktop-version/Module7_MEAN_Stack/project/code/m7p-ngfe/src/app/app.ts
+++ local/canadian-business-college/desktop-version/Module7_MEAN_Stack/project/code/m7p-ngfe/src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('m7p-ngfe');
+  protected readonly title = signal('Module 7 Project');
 }
