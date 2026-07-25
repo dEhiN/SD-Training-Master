484de2a8de8660b4b6be7c0cc531f0de6ab3257e
Hiran Watson <hiranwatson@gmail.com>
2026-07-25 01:29:50 -0400

    Finished Part 1

---
+++ local/canadian-business-college/desktop-version/Module8_Python_Flask/assignments/2/code/assignment_two.py
@@ -0,0 +1,22 @@
+# This is my Module 8, Assignment 2 for the CBC FSWAD diploma
+
+"""
+Part 1: Lists
+This section is to deal with lists and consists of the following instructions:
+
+1. Create a list called fruits with the following values: "apple", "banana", "cherry", "mango".
+
+2. Print the second item in the list.
+
+3. Add "orange" to the list.
+
+4. Change the first item of the list to "grape".
+
+5. Print the length of the list.
+"""
+
+fruits = ["apple", "banana", "cherry", "mango"]
+print(fruits[1])
+fruits.append("orange")
+fruits[0] = "grape"
+print(len(fruits))
