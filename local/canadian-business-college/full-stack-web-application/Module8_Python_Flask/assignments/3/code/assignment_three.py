1823fab95f0919b50d003aeb50eb07665303ad3c
Hiran Watson <hiranwatson@gmail.com>
2026-08-02 19:55:32 -0400

    Finished part 1

---
+++ local/canadian-business-college/full-stack-web-application/Module8_Python_Flask/assignments/3/code/assignment_three.py
@@ -0,0 +1,19 @@
+# This is my Module 8, Assignment 2 for the CBC FSWAD diploma
+
+"""
+Part 1: Functions Without Parameters
+1. Write a function called greet_user that prints: Hello, welcome to Python class!
+
+2. Write a function called show_python_tip that prints: Tip: Use clear variable names in your code.
+
+3. Test it: Call both functions to display their messages on the screen.
+"""
+
+def greet_user():
+    print("Hello, welcome to Python class!")
+
+def show_python_tip():
+    print("Tip: Use clear variable names in your code.")
+
+greet_user()
+show_python_tip()
