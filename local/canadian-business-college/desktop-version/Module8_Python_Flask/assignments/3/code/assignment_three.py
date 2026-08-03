b35d4ae8bf8d3ab99585038308279c4322387d35
Hiran Watson <hiranwatson@gmail.com>
2026-08-02 23:22:12 -0400

    Finished part 3

--- local/canadian-business-college/desktop-version/Module8_Python_Flask/assignments/3/code/assignment_three.py
+++ local/canadian-business-college/desktop-version/Module8_Python_Flask/assignments/3/code/assignment_three.py
@@ -1,5 +1,8 @@
 # This is my Module 8, Assignment 2 for the CBC FSWAD diploma
 
+import math
+import random
+
 """
 Part 1: Functions Without Parameters
 
@@ -41,3 +44,32 @@ print("\nPart 2: Functions With Parameters")
 greet_by_name("David")
 sum_result = add_numbers(15, 20)
 print(sum_result)
+
+
+"""
+Part 3: Using Built-in Packages
+
+1. Use the math module:
+
+- Import the math module at the top of your file.
+
+- Write a function called find_square_root(number) that returns the square root of the number using math.sqrt(). (Example: find_square_root(16) → 4.0)
+
+2. Use the random module:
+
+- Import the random module at the top of your file.
+
+- Write a function called roll_dice() that returns a random number between 1 and 6 using random.randint().
+
+3. Test it: Call both functions and print their results. (Example output for dice: You rolled a 4)
+"""
+
+def find_square_root(number):
+    return math.sqrt(number)
+
+def roll_dice():
+    return random.randint(1, 6)
+
+print("\nPart 3: Using Built-in Packages")
+print(find_square_root(16))
+print(roll_dice())
